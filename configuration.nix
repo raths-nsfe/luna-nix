a6b49600a49b0e95eb702dc06617ba285b362513
nsfe <raths.work@gmail.com>
2025-06-03 19:57:06 +1000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,7 @@
 {
   #bootloader
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "dev/sda";
+  boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
   networking = {
