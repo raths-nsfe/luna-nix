9c0319ba94f9f867ab39dd5b5d18802e8ca64a53
nsfe <raths.work@gmail.com>
2025-06-03 20:01:39 +1000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,14 @@
 { config, pkgs, ... }:
 
 {
+  fileSystems."/data" =
+  {
+    device = "/dev/sda";
+    fsType = "ext4";
+  };
   #bootloader
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "dev/sda";
   boot.loader.grub.useOSProber = true;
 
   networking = {
