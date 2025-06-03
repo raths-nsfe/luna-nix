3da04ca2e57fef479df7ad3de90e30e979c12b86
nsfe <raths.work@gmail.com>
2025-06-03 19:15:25 +1000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -12,57 +12,10 @@
       allowedTCPPorts = [];
       allowedUDPPorts = [];
     };
-    hostName = "aya";
+    hostName = "luna";
     networkmanager.enable = true;
 
   };
-
-  services = {
-
-    openssh.enable = true;
-
-    redshift = {
-      enable = true;
-      latitude = "35";
-      longitude = "139";
-    };
-
-    ntp.enable = true;
-    nscd.enable = false;
-
-    xserver = {
-      videoDrivers = [ "nvidia" ];
-      enable = true;
-      layout = "us";
-
-      xkbOptions = "compose:caps";
-      xkbVariant = "dvp";
-
-      synaptics.enable = true;
-
-      windowManager.xmonad.enable = true;
-      windowManager.xmonad.extraPackages = self: [ self.xmonad-contrib ];
-      windowManager.xmonad.haskellPackages = pkgs.haskell.packages.ghc822;
-      windowManager.default = "xmonad";
-      desktopManager.default = "none";
-
-      displayManager.lightdm = {
-        enable = true;
-        extraSeatDefaults = ''
-          greeter-show-manual-login=true
-          greeter-hide-users=true
-          allow-guest=false
-        '';
-      };
-
-      displayManager.sessionCommands = ''
-        ${pkgs.xlibs.xsetroot}/bin/xsetroot -cursor_name left_ptr
-        ${pkgs.xscreensaver}/bin/xscreensaver -no-splash &
-      '';
-    };
-  };
-
-  users.extraGroups.vboxusers.members = [ "shana" ];
 
   programs.ssh.startAgent = true;
   programs.bash.enableCompletion = true;
@@ -70,12 +23,11 @@
   # Don't blind me
   systemd.services.redshift.restartIfChanged = false;
 
-  time.timeZone = "Asia/Tokyo";
+  time.timeZone = "Australia/Melbourne";
 
   nixpkgs.system = "x86_64-linux";
   nixpkgs.config = {
     virtualbox.enableExtensionPack = true;
-    pulseaudio = true;
     allowUnfree = true;
   };
 
